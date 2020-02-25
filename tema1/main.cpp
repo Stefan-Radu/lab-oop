9ce8707fc7a30fd3ca2d30727c33b261dfaacb62
Stefan Radu <stefan270101@gmail.com>
2020-02-25 22:51:56 +0200

    finalized and tested

--- tema1/main.cpp
+++ tema1/main.cpp
@@ -12,8 +12,15 @@ public:
   } // constructor
 
   ~Node() {
-    /* if (leftSon) delete leftSon; */
-    /* if (rightSon) delete rightSon; */
+    if (leftSon) {
+      delete leftSon;
+      leftSon = nullptr;
+    }
+    if (rightSon) {
+      delete rightSon;
+      leftSon = nullptr;
+    }
+    cerr << "Deleted the value " << info << ' ' << "in destructor\n";
   } // destructor
 
   int info;
@@ -28,74 +35,46 @@ public:
   } // constructor
   
   ~ABC() {
-    m_recursiveErase(root);
+    delete root;
+    root = nullptr;
   }
 
   void operator + (int value) {
     m_insert(value, root);
   }
 
-  ABC* operator >> (int value) {
+  ABC& operator >> (int value) {
     m_insert(value, root);
-    return this;
+    return *this;
   }
 
-  ofstream& operator << (ofstream &out) {
-    out << "In order print:\n";
-    m_inOrderPrint(root, out);
-    out << "Preorder print:\n";
-    m_preOrderPrint(root, out);
-    out << "Postorder print:\n";
-    m_postOrderPrint(root, out);
-    return out;
+  void operator - (int value) {
+    m_findForErase(value, root);
   }
 
-  bool erase(int value) {
-
-    Node *node = find(value, root);
-    if (not node) return false;
-
-    Node *nextNode;
-    while (node -> rightSon) {
-
-      nextNode = node -> rightSon;
-      while (nextNode -> leftSon) {
-        nextNode = nextNode -> leftSon;
-      }
-
-      swap(node -> info, nextNode -> info);
-      node = nextNode;
-    }
-
-    delete node;
-    return true;
+  void erase(int value) {
+    m_findForErase(value, root);
   }
 
   int getDepth() {
     return m_getDepth(root);
   }
 
-  void printLeaves(ofstream &out) {
+  void printLeaves(ostream &out) {
+    out << "Leaves of the tree are: ";
     m_printLeaves(root, out);
+    out << "\n\n";
   }
 
 private:
 
   Node *root;
 
-  void m_recursiveErase(Node *currentNode) {
-    if (not currentNode) return;
-
-    m_recursiveErase(currentNode -> leftSon);
-    m_recursiveErase(currentNode -> rightSon);
-
-    delete currentNode;
-  }
-
-  void m_insert(int value, Node *currentNode) {
+  void m_insert(int value, Node *&currentNode) {
 
     if (not currentNode) {
       currentNode = new Node(value);
+      cerr << "Inserted the value " << value << '\n';
       return;
     }
 
@@ -108,7 +87,7 @@ private:
   }
 
 
-  void m_inOrderPrint(Node *currentNode, ofstream &out) {
+  void m_inOrderPrint(Node *currentNode, ostream &out) {
 
     if (not currentNode) {
       return;
@@ -119,7 +98,7 @@ private:
     m_inOrderPrint(currentNode -> rightSon, out);
   }
 
-  void m_preOrderPrint(Node *currentNode, ofstream &out) {
+  void m_preOrderPrint(Node *currentNode, ostream &out) {
 
     if (not currentNode) {
       return;
@@ -130,7 +109,7 @@ private:
     m_preOrderPrint(currentNode -> rightSon, out);
   }
 
-  void m_postOrderPrint(Node *currentNode, ofstream &out) {
+  void m_postOrderPrint(Node *currentNode, ostream &out) {
 
     if (not currentNode) {
       return;
@@ -141,34 +120,57 @@ private:
     out << currentNode -> info << ' ';
   }
 
-  Node *find(int value, Node *currentNode) {
+  void m_findForErase(int value, Node *&currentNode) {
 
     if (not currentNode) {
-      return nullptr;
+      return;
     }
 
     if (value < currentNode -> info) {
-      return find(value, currentNode -> leftSon);
+      m_findForErase(value, currentNode -> leftSon);
+      return;
     }
     if (value > currentNode -> info) {
-      return find(value, currentNode -> rightSon);
+      m_findForErase(value, currentNode -> rightSon);
+      return;
     }
 
-    return currentNode;
+    cerr << "Planning to delete " << currentNode -> info << '\n';
+    m_erase(currentNode);
+  }
+
+  void m_erase(Node *&currentNode) {
+
+    if (not currentNode -> rightSon) {
+      auto aux = currentNode -> leftSon;
+      currentNode -> leftSon = nullptr;
+      delete currentNode;
+      currentNode = aux;
+      return;
+    }
+
+    Node *& nextNode = currentNode -> rightSon;
+    while (nextNode -> leftSon) {
+      nextNode = nextNode -> leftSon;
+    }
+
+    swap(currentNode -> info, nextNode -> info);
+    auto aux = nextNode -> rightSon;
+    nextNode -> rightSon = nullptr;
+    delete nextNode;
+    nextNode = aux;
   }
 
   int m_getDepth(Node *curNode) {
-
-    if (not curNode) {
-      return 0;
-    }
-
+    if (not curNode) return -1;
     return max(m_getDepth(curNode -> leftSon), m_getDepth(curNode -> rightSon)) + 1;
   }
 
-  void m_printLeaves(Node *curNode, ofstream &out) {
+  void m_printLeaves(Node *curNode, ostream &out) {
 
-    if (curNode -> rightSon == curNode -> rightSon and curNode -> rightSon == nullptr) {
+    if (not curNode) return;
+
+    if (curNode -> leftSon == curNode -> rightSon and curNode -> rightSon == nullptr) {
       out << curNode -> info << ' ';
       return;
     }
@@ -176,8 +178,69 @@ private:
     m_printLeaves(curNode -> leftSon, out);
     m_printLeaves(curNode -> rightSon, out);
   }
+
+  friend ostream& operator << (ostream &os, ABC &abc);
 };
+
+ostream& operator << (ostream &os, ABC &abc) {
+  os << "\nIn order print:\n";
+  abc.m_inOrderPrint(abc.root, os);
+  os << "\n\nPreorder print:\n";
+  abc.m_preOrderPrint(abc.root, os);
+  os << "\n\nPostorder print:\n";
+  abc.m_postOrderPrint(abc.root, os);
+  os << '\n';
+  return os;
+}
+
 
 int main() {
 
+  ABC abc;
+
+  // testing insert;
+
+  /* abc + 5; */
+  /* abc + 2; */
+  /* abc + 8; */
+  /* abc + 10; */
+  /* abc + 9; */
+  /* abc + 1; */
+  /* abc + 3; */
+  /* abc + 2; */
+  /* abc + 2; */
+
+  // testing overloaded insert
+
+  abc >> 5 >> 2 >> 8 >> 10 >> 9 >> 1 >> 3 >> 2 >> 2;
+
+  // testing print function
+
+  cerr << abc << '\n';
+
+  // testing erase
+
+  /* abc.erase(10); */
+  /* cerr << abc << '\n'; */
+  /* abc.erase(5); */
+  /* cerr << abc << '\n'; */
+  /* abc.erase(2); */
+  /* cerr << abc << '\n'; */
+
+  // testing erase with overload
+
+  abc - 10;
+  cerr << abc << '\n';
+  abc - 5;
+  cerr << abc << '\n';
+  abc - 2;
+  cerr << abc << '\n';
+
+  // testing depth function
+
+  cerr << "Depth of the tree is " << abc.getDepth() << "\n\n";
+
+  // test print leaves function
+
+  abc.printLeaves(cerr);
 }
