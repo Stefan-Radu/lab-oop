7ca0f5fe64bd30211cbc63ff5d9aed547e1dfc8d
Stefan Radu <stefan270101@gmail.com>
2020-03-19 10:45:26 +0200

    basic functionality of creature class

--- 2nd_assignment/Creature.cpp
+++ 2nd_assignment/Creature.cpp
@@ -1,14 +1,46 @@
 #include "Creature.hpp"
 
+//==================================================  Vec2D  ==================================================
 Vec2D::Vec2D(): x(0), y(0) {}
 Vec2D::Vec2D(int x, int y): x(x), y(y) {}
+Vec2D::Vec2D(const Vec2D &v): x(v.x), y(v.y) {}
 
 void Vec2D::operator = (const Vec2D &other) {
   this -> x = other.x;
   this -> y = other.y;
 }
 
+Vec2D Vec2D::generateRAndomVec2D() {
+  return Vec2D(rand() % 3 - 1, rand() % 3 - 1);
+}
+
 std::ostream& operator << (std::ostream &os, Vec2D &v) {
   os << v.x << ' ' << v.y;
   return os;
+}
+
+//==================================================  Creature  ==================================================
+
+Creature::Creature(int h, Vec2D pos): health(h), position(pos) {}  
+
+bool Creature::isAlive() const {
+  return health != 0;
+}
+
+//==================================================  Prey  ==================================================
+
+Prey::Prey(): Creature(1, Vec2D()) {}
+Prey::Prey(Vec2D pos): Creature(1, pos) {}
+
+void Prey::updateHealth() {
+  health += HEALTH_TIC;
+}
+
+//==================================================  Predator  ==================================================
+
+Predator::Predator(): Creature(MAX_HEALTH, Vec2D()) {}
+Predator::Predator(Vec2D pos): Creature(MAX_HEALTH, pos) {}
+
+void Predator::updateHealth() {
+  health -= HEALTH_TIC;
 }
--- 2nd_assignment/Creature.hpp
+++ 2nd_assignment/Creature.hpp
@@ -3,10 +3,15 @@
 #include <iostream>
 
 struct Vec2D {
+
   Vec2D();
   Vec2D(int, int);
+  Vec2D(const Vec2D&);
+
   int x, y;
+
   void operator = (const Vec2D&);
+  static Vec2D generateRAndomVec2D();
 };
 
 std::ostream& operator << (std::ostream&, Vec2D&);
@@ -14,13 +19,43 @@ std::ostream& operator << (std::ostream&, Vec2D&);
 class Creature {
 public:
 
-  Creature();
+  Creature(int, Vec2D);
+
+  virtual void updateHealth() = 0;
+  bool isAlive() const; 
+
+  void updatePosition();
+
+protected:
+  int health;
+  Vec2D position;
+};
+
+class Prey: public Creature {
+public:
+
+  Prey();
+  Prey(Vec2D);
+
+  void updateHealth();
 
 private:
 
-
-
-
+  static const int MAX_HEALTH = 100;
+  static const int HEALTH_TIC = 1;
 };
 
+class Predator: public Creature {
+public:
+
+  Predator();
+  Predator(Vec2D);
+
+  void updateHealth();
+
+private:
+
+  static const int MAX_HEALTH = 100;
+  static const int HEALTH_TIC = 1;
+};
 
