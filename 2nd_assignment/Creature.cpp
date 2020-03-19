dae7fa7c9cc9da0e2372ee2cdec5228196d00fc8
Stefan Radu <stefan270101@gmail.com>
2020-03-20 00:04:28 +0200

    added all functionality

--- 2nd_assignment/Creature.cpp
+++ 2nd_assignment/Creature.cpp
@@ -1,22 +1,32 @@
 #include "Creature.hpp"
 
 //==================================================  Vec2D  ==================================================
-Vec2D::Vec2D(): x(0), y(0) {}
-Vec2D::Vec2D(int x, int y): x(x), y(y) {}
-Vec2D::Vec2D(const Vec2D &v): x(v.x), y(v.y) {}
+Vec2D::Vec2D(): i(0), j(0) {}
+Vec2D::Vec2D(int i, int j): i(i), j(j) {}
+Vec2D::Vec2D(const Vec2D &v): i(v.i), j(v.j) {}
 
 Vec2D& Vec2D::operator = (const Vec2D &other) {
-  this -> x = other.x;
-  this -> y = other.y;
+  this -> i = other.i;
+  this -> j = other.j;
   return *this;
 }
 
-Vec2D Vec2D::generateRandomVec2D() {
-  return Vec2D(rand() % 3 - 1, rand() % 3 - 1);
+Vec2D Vec2D::operator + (const Vec2D &other) const {
+  return Vec2D(this -> i + other.i, this -> j + other.j);
 }
 
-std::ostream& operator << (std::ostream &os, Vec2D &v) {
-  os << v.x << ' ' << v.y;
+void Vec2D::operator += (const Vec2D &other) {
+  this -> i += other.i;
+  this -> j += other.j;
+}
+
+Vec2D Vec2D::getRandomVec2D() {
+  auto aux = Vec2D(rand() % 3 - 1, rand() % 3 - 1);
+  return aux;
+}
+
+std::ostream& operator << (std::ostream &os, const Vec2D &v) {
+  os << v.i << ' ' << v.j;
   return os;
 }
 
@@ -28,11 +38,27 @@ bool Creature::isAlive() const {
   return health != 0;
 }
 
+int Creature::getHealth() const {
+  return health;
+}
+
+Vec2D Creature::getPosition() const {
+  return position;
+}
+
+void Creature::updatePosition(const int &limitH, const int &limitW) {
+  position += Vec2D::getRandomVec2D();
+  if (position.i < 0) position.i = limitH - 1;
+  else if (position.i == limitH) position.i = 0;
+  if (position.j < 0) position.j = limitW - 1;
+  else if (position.j == limitW) position.j = 0;
+}
+
 //==================================================  Prey  ==================================================
 
 Prey::Prey(): Creature(1, Vec2D()) {}
 Prey::Prey(Vec2D pos): Creature(1, pos) {}
-Prey::Prey(const Prey& prey): Creature(1, prey.position) {}
+Prey::Prey(const Prey& prey): Creature(prey.health, prey.position) {}
 
 Prey& Prey:: operator = (const Prey &other) {
   health = other.health;
@@ -41,14 +67,22 @@ Prey& Prey:: operator = (const Prey &other) {
 }
 
 void Prey::updateHealth() {
-  health += HEALTH_TIC;
+  health = std::min(health + HEALTH_TIC, (int) MAX_HEALTH);
+}
+
+bool Prey::canReproduce() const {
+  return health == MAX_HEALTH;
+}
+
+void Prey::resetHealth() {
+  health = 1;
 }
 
 //==================================================  Predator  ==================================================
 
 Predator::Predator(): Creature(MAX_HEALTH, Vec2D()) {}
 Predator::Predator(Vec2D pos): Creature(MAX_HEALTH, pos) {}
-Predator::Predator(const Predator& predator): Creature(MAX_HEALTH, predator.position) {}
+Predator::Predator(const Predator& predator): Creature(predator.health, predator.position) {}
 
 Predator& Predator:: operator = (const Predator &other) {
   health = other.health;
@@ -58,4 +92,12 @@ Predator& Predator:: operator = (const Predator &other) {
 
 void Predator::updateHealth() {
   health -= HEALTH_TIC;
+}
+
+void Predator::updateHealth(int toAdd) {
+  health = std::max(health + toAdd, (int) MAX_HEALTH);
+}
+
+bool Predator::canReproduce() const {
+  return health == MAX_HEALTH;
 }
--- 2nd_assignment/Creature.hpp
+++ 2nd_assignment/Creature.hpp
@@ -7,14 +7,17 @@ struct Vec2D {
   Vec2D();
   Vec2D(int, int);
   Vec2D(const Vec2D&);
+
   Vec2D& operator = (const Vec2D&);
+  Vec2D operator + (const Vec2D&) const;
+  void operator += (const Vec2D&);
 
-  int x, y;
+  int i, j;
 
-  static Vec2D generateRandomVec2D(); // for moving a creature 1 step up / down/ left / right
+  static Vec2D getRandomVec2D(); // for moving a creature 1 step up / down/ left / right
 };
 
-std::ostream& operator << (std::ostream&, Vec2D&);
+std::ostream& operator << (std::ostream&, const Vec2D&);
 
 class Creature {
 public:
@@ -22,9 +25,13 @@ public:
   Creature(int, Vec2D);
 
   virtual void updateHealth() = 0;
-  bool isAlive() const; 
 
-  void updatePosition();
+  bool isAlive() const; 
+  int getHealth() const;
+  virtual bool canReproduce() const = 0;
+
+  Vec2D getPosition() const;
+  void updatePosition(const int&, const int&);
 
 protected:
   int health;
@@ -39,11 +46,13 @@ public:
   Prey(const Prey&);
   Prey& operator = (const Prey&);
 
+  bool canReproduce() const override;
   void updateHealth() override;
+  void resetHealth();
 
 private:
 
-  static const int MAX_HEALTH = 100;
+  static const int MAX_HEALTH = 2;
   static const int HEALTH_TIC = 1;
 };
 
@@ -55,10 +64,12 @@ public:
   Predator(const Predator&);
   Predator& operator = (const Predator&);
 
+  bool canReproduce() const override;
   void updateHealth() override;
+  void updateHealth(int);
 
 private:
 
-  static const int MAX_HEALTH = 100;
+  static const int MAX_HEALTH = 2;
   static const int HEALTH_TIC = 1;
 };
