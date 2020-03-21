b10a11445978f4a580ebabdda7b31beef20aab3f
Stefan Radu <stefan270101@gmail.com>
2020-03-21 10:14:27 +0200

    added div arrays

--- 2nd_assignment/Vec2D.hpp
+++ 2nd_assignment/Vec2D.hpp
@@ -14,6 +14,9 @@ struct Vec2D {
 
   int i, j;
 
+  const int dirI[4] = {0, 0, 1, -1};
+  const int dirJ[4] = {1, -1, 0, 0};
+
   static Vec2D getRandomWay(); // for moving a creature 1 step up / down/ left / right
 };
 
