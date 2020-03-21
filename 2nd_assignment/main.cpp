b76d3f1093749c6608c02b84dd9f3766e7a7e007
Stefan Radu <stefan270101@gmail.com>
2020-03-21 12:58:37 +0200

    Can now create a const number of game objects and run them. Also added a few todos

--- 2nd_assignment/main.cpp
+++ 2nd_assignment/main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include "Game.hpp"
-#include "Creature.hpp"
 
 int main () {
-  Game *g = new Game(200, 200, 50, 50);
-  g -> run();
-  delete g;
+
+  const int GAMES_CNT = 7;
+  Game **g = new Game*[GAMES_CNT];
+
+  for (int gameCnt = 0; gameCnt < GAMES_CNT; ++ gameCnt) {
+
+    int preyPerc = rand() % 50 + 1;
+    int predatorPerc = rand() % 50 + 1;
+    int preyMaxHealth = rand() % 100 + 1;
+    int preyHealthTic = rand() % 5 + 1;
+    int predatorMaxHealth = rand() % 100 + 1;
+    int predatorHealthTic = rand() % 5 + 1;
+
+    g[gameCnt] = new Game(preyPerc, predatorPerc,
+                    preyMaxHealth, preyHealthTic,
+                    predatorMaxHealth, predatorHealthTic);
+
+    std::cerr << "\nInitialized game no. " << gameCnt << " with:\n";
+    std::cerr << "Prey percentage: " << predatorPerc << " / 1000\n";
+    std::cerr << "Predator percentage: " << predatorPerc << " / 1000\n";
+    std::cerr << "Maximum health of prey: " << preyMaxHealth <<  '\n';
+    std::cerr << "Health tic of prey (+- 1): " << preyHealthTic <<  '\n';
+    std::cerr << "Maximum health of predator: " << predatorMaxHealth <<  '\n';
+    std::cerr << "Health tic of predator (+- 1): " << predatorHealthTic <<  '\n';
+
+    g[gameCnt] -> run();
+    delete g[gameCnt];
+  }
+
+  delete[] g;
+
+  // TODO implement nuke / disease to killall
+  // TODO assert limits
 }
