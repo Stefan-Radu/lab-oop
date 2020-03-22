ff34a11da4ee09b0cd7fd096b7810af2a3b5e633
Stefan Radu <stefan270101@gmail.com>
2020-03-22 15:30:20 +0200

    changed percentages

--- 2nd_assignment/main.cpp
+++ 2nd_assignment/main.cpp
@@ -4,16 +4,18 @@
 
 int main () {
 
+  srand(time(0));
+
   const int GAMES_CNT = 7;
   Game **g = new Game*[GAMES_CNT];
 
   for (int gameCnt = 0; gameCnt < GAMES_CNT; ++ gameCnt) {
 
-    int preyPerc = rand() % 50 + 1;
-    int predatorPerc = rand() % 50 + 1;
-    int preyMaxHealth = rand() % 100 + 1;
+    int preyPerc = rand() % 10 + 1;
+    int predatorPerc = rand() % 10 + 1;
+    int preyMaxHealth = rand() % 50 + 1;
     int preyHealthTic = rand() % 5 + 1;
-    int predatorMaxHealth = rand() % 100 + 1;
+    int predatorMaxHealth = rand() % 50 + 1;
     int predatorHealthTic = rand() % 5 + 1;
 
     g[gameCnt] = new Game(preyPerc, predatorPerc,
@@ -21,8 +23,8 @@ int main () {
                     predatorMaxHealth, predatorHealthTic);
 
     std::cerr << "\nInitialized game no. " << gameCnt << " with:\n";
-    std::cerr << "Prey percentage: " << predatorPerc << " / 1000\n";
-    std::cerr << "Predator percentage: " << predatorPerc << " / 1000\n";
+    std::cerr << "Prey percentage: " << predatorPerc << " / " << Game::CHANCE_MODULO << '\n';
+    std::cerr << "Predator percentage: " << predatorPerc << " / " << Game::CHANCE_MODULO << '\n';
     std::cerr << "Maximum health of prey: " << preyMaxHealth <<  '\n';
     std::cerr << "Health tic of prey (+- 1): " << preyHealthTic <<  '\n';
     std::cerr << "Maximum health of predator: " << predatorMaxHealth <<  '\n';
@@ -34,6 +36,8 @@ int main () {
 
   delete[] g;
 
-  // TODO implement nuke / disease to killall
   // TODO assert limits
+  // TODO remake redame 
+  // TODO git tags
+  // TODO git release notes
 }
